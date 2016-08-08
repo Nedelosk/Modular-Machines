ca30315c45efa8e644b82b8c46c365532bcf6432
Nedelosk <nedelosk@gmail.com>
2016-08-08 17:33:57 +0200

    Remove tickable interface from casing

--- src/main/java/de/nedelosk/modularmachines/api/modules/IModuleCasing.java
+++ src/main/java/de/nedelosk/modularmachines/api/modules/IModuleCasing.java
@@ -3,6 +3,6 @@ package de.nedelosk.modularmachines.api.modules;
 import de.nedelosk.modularmachines.api.modules.energy.IModuleHeat;
 import de.nedelosk.modularmachines.api.modules.storaged.IModuleModuleStorage;
 
-public interface IModuleCasing extends IModuleModuleStorage, IModuleTickable, IModuleHeat {
+public interface IModuleCasing extends IModuleModuleStorage {
 
 }
