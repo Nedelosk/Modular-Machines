60a14c1973ee418b5e57fec0eef6efe1664768fb
Nedelosk <nedelosk@gmail.com>
2016-08-17 13:21:34 +0200

    Fix mcmod.info

--- src/main/java/de/nedelosk/modularmachines/common/core/Constants.java
+++ src/main/java/de/nedelosk/modularmachines/common/core/Constants.java
@@ -3,7 +3,7 @@ package de.nedelosk.modularmachines.common.core;
 public class Constants {
 
 	public static final String MODID = "modularmachines";
-	public static final String NAME = "Modular-Machine's";
+	public static final String NAME = "Modular Machine's";
 	public static final String VERSION = "@VERSION@";
 	public static final String DEPENDENCIES = "required-after:Forge@[12.18.1.2045,);" 
 			+ "after:JEI@[3.7.8,);";
