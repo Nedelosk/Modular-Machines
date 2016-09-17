f41ffe1ecb18ec78dff53d4692ef0ee9837fcd9d
Nedelosk <nedelosk@gmail.com>
2016-09-18 00:52:43 +0200

    Fix casing modules

--- src/main/java/de/nedelosk/modularmachines/api/modules/models/ModelHandlerCasing.java
+++ src/main/java/de/nedelosk/modularmachines/api/modules/models/ModelHandlerCasing.java
@@ -6,12 +6,16 @@ import java.util.Set;
 
 import com.google.common.base.Function;
 
+import de.nedelosk.modularmachines.api.modules.EnumModuleSizes;
 import de.nedelosk.modularmachines.api.modules.IModelInitHandler;
+import de.nedelosk.modularmachines.api.modules.IModule;
 import de.nedelosk.modularmachines.api.modules.items.IModuleContainer;
 import de.nedelosk.modularmachines.api.modules.position.EnumStoragePositions;
 import de.nedelosk.modularmachines.api.modules.position.IStoragePosition;
 import de.nedelosk.modularmachines.api.modules.state.IModuleState;
+import de.nedelosk.modularmachines.api.modules.state.IModuleStateClient;
 import de.nedelosk.modularmachines.api.modules.storage.IStorage;
+import de.nedelosk.modularmachines.api.modules.storage.module.IModuleStorage;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.util.ResourceLocation;
@@ -53,6 +57,12 @@ public class ModelHandlerCasing extends ModelHandler implements IModelHandler, I
 			}
 		}
 		models.add(getBakedModel(casing, modelState, format, bakedTextureGetter));
+		for(IModuleState moduleState : ((IModuleStorage)storage).getModules()){
+			IModule module = moduleState.getModule();
+			if(((IModuleStateClient)moduleState).getModelHandler() != null){
+				models.add(ModuleModelHelper.getModel(moduleState, storage, modelState, format));
+			}
+		}
 		bakedModel = new BakedMultiModel(models);
 	}
 }
