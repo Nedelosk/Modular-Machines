a787d365c5cc973b7ef5fb1b610235c69c93836c
Nedelosk <nedelosk@gmail.com>
2016-10-02 19:38:30 +0200

    Fix module cleaner

--- src/main/java/de/nedelosk/modularmachines/api/modules/IModuleModuleCleaner.java
+++ src/main/java/de/nedelosk/modularmachines/api/modules/IModuleModuleCleaner.java
@@ -2,7 +2,7 @@ package de.nedelosk.modularmachines.api.modules;
 
 import de.nedelosk.modularmachines.api.modules.state.IModuleState;
 
-public interface IModuleModuleCleaner extends ITickable {
+public interface IModuleModuleCleaner extends ITickable, IModule {
 
 	void cleanModule(IModuleState state);
 }
